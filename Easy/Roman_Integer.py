6e5642f0e0a6245b1e205acc902e5193e07cc08f
Joel John <79360318+joeljohn135@users.noreply.github.com>
2022-06-15 19:26:34 +0400

    Update Roman_Integer.py

--- Easy/Roman_Integer.py
+++ Easy/Roman_Integer.py
@@ -1,14 +1,11 @@
-class Solution(object):
-    
-  def romanToInt(self, s):
-        dicti = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        previous = 0
+#python3
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        conv = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
         sum = 0
-        for i in s[::-1]:
-            current = dicti[i]
-            if previous > current:
-                sum -= current
-            else:
-                sum += current
-            prev = current
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            sum +=  conv[char]
         return sum
