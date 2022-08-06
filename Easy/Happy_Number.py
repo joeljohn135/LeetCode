d2ee3eac459176cadedcb0a26461cfd9c9961cf8
Joel John <79360318+joeljohn135@users.noreply.github.com>
2022-08-06 12:50:31 +0400

    Update Happy_Number.py

--- Easy/Happy_Number.py
+++ Easy/Happy_Number.py
@@ -6,3 +6,12 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        i=0
+        while n!=1 and i<50:
+            quo=0
+            while n!=0:
+                quo+=(n%10)**2
+                n//=10
+                i+=1
+            n=quo
+        return n==1
