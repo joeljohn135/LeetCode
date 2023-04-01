c3b88684b07dd29ea6086c973266d1f0380a7ac0
Zachery Marksman <129591468+Devexpert10@users.noreply.github.com>
2023-04-01 14:23:31 +0400

    Optimized ver
    Here is an optimized version hope it helps ;)

--- Easy/strStr_Implement.py
+++ Easy/strStr_Implement.py
@@ -5,12 +5,40 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        if not needle:
+            return 0
+
         n = len(needle)
-        h = len(haystack)
-        if h < n:
-            return -1
-        else:
-            for i in range(h-n+1):
-                if haystack[i:i+n] == needle:
-                    return i
-            return -1
+        prefix_suffix = self.get_prefix_suffix(needle)
+
+        i, j = 0, 0
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+                if j == n:
+                    return i - n
+            elif j > 0:
+                j = prefix_suffix[j-1]
+            else:
+                i += 1
+
+        return -1
+
+    def get_prefix_suffix(self, needle):
+        n = len(needle)
+        prefix_suffix = [0] * n
+
+        i, j = 1, 0
+        while i < n:
+            if needle[i] == needle[j]:
+                j += 1
+                prefix_suffix[i] = j
+                i += 1
+            elif j > 0:
+                j = prefix_suffix[j-1]
+            else:
+                prefix_suffix[i] = 0
+                i += 1
+
+        return prefix_suffix
